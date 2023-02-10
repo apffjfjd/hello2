d1311141949773d9849f2d321541453e52a24229
apffjfjd <115337453+apffjfjd@users.noreply.github.com>
2023-02-10 18:26:02 +0900

    crudv1.1.0.re

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -6,6 +6,8 @@ const videoRouter = express.Router();
 
 videoRouter.route("/upload").get(uploadVideos).post(postUpload); // videos/upload
 videoRouter.route("/list").get(list).post(list); // videos/list
+videoRouter.route("/list").get(list); // videos/edit
+// videoRouter.route("/edit").get(edit).post(postUpdate); // videos/list
 
 // globalRouter.get("/join", join);
 // globalRouter.get("/login", login);
